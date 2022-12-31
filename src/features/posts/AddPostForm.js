8c34797f101f93d3508f07f9e26e4bab1deac0a0
Nazar Diachok <diafcc55@gmail.com>
2022-12-31 20:44:52 +0100

    ´+

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { postAdded } from "./postSlice";
+
 function AddPostForm() {
+    const[title,setTitle]=useState("");
+    const[content,setContent]=useState("");
+    const dispatch = useDispatch();
+
+    function onTitleChanged(e){
+        setTitle(e.target.value)
+    }
+    function onContentChanged(e){
+        setContent(e.target.value)
+    }
+
+    function onSavePostClicked(){
+        if(title && content){
+            dispatch(postAdded({
+                id:nanoid(),
+                title,
+                content}))
+
+            setTitle("");
+            setContent("")
+        }
+    }
+
     return ( 
         <div className="">
-            <h2>Add Post Form</h2>
-            <form>
-                <label htmlFor="postTitle"></label>
+            <h2 className="flex mx-auto justify-center">Add Post Form:</h2>
+            <form className="flex p-1 w-96 mx-auto flex-col">
+                <label htmlFor="postTitle">Post Title: </label>
+                <input className="border" type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
+
+                <label htmlFor="postTitle">Post Title: </label>
+                <textarea className="border" id="postContent" name="postContent" value={content} onChange={onContentChanged} />
+
+                <button className=" flex border bg-cyan-800 w-20 mx-auto mt-2" type="button" onClick={()=>onSavePostClicked()}>Save Post</button>
             </form>
         </div>
      );
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,19 +1,24 @@
 import { useSelector } from "react-redux";
+import AddPostForm from "./AddPostForm";
 
 function PostsList() {
     const posts = useSelector(state=> state.posts)
     return ( 
-    <div className="flex p-10 w-96 mx-auto flex-col">
+        <>
+        <AddPostForm></AddPostForm>
+        <div className="flex p-10 w-96 mx-auto flex-col">
         <div className="font-bold p-5">Posts: </div>
         <div className="">
             {posts.map(post=>
             <div className="border rounded-lg p-5 mt-5" key={post.id}>
                 <h3>{post.title}</h3>
-                <p>{post.content.substring(0,100)}</p>
+                <p>{post.content}</p>
                 
             </div> )}
         </div>
     </div> 
+        </>
+    
     );
 }
 
